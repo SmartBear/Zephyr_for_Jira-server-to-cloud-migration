7e6a53471ce076cb83cbef9b9136309fe62192ab
Harsh Dubey <harsh.dubey@smartbear.com>
2021-08-12 10:43:15 +0530

    Added service class.

--- src/main/java/com/zephyr/migration/service/DefectLinkService.java
+++ src/main/java/com/zephyr/migration/service/DefectLinkService.java
@@ -1,2 +1,4 @@
-package com.zephyr.migration.service;public interface DefectLinkService {
+package com.zephyr.migration.service;
+
+public interface DefectLinkService {
 }
--- src/main/java/com/zephyr/migration/service/impl/DefectLinkServiceImpl.java
+++ src/main/java/com/zephyr/migration/service/impl/DefectLinkServiceImpl.java
@@ -1,2 +1,8 @@
-package com.zephyr.migration.service.impl;public class DefectLinkServiceImpl {
+package com.zephyr.migration.service.impl;
+
+import com.zephyr.migration.service.DefectLinkService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DefectLinkServiceImpl implements DefectLinkService {
 }
