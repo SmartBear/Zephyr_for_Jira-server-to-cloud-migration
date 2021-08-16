fada387a299eaafcfe13a07d0794c1ea7fe7dc77
Agarwal Himanshu <agarwal.himanshu@smartbear.com>
2021-08-16 16:26:12 +0530

    Fixed - ZFJCLOUD-17339

---
+++ src/main/java/com/zephyr/migration/model/Defect.java
@@ -0,0 +1,15 @@
+package com.zephyr.migration.model;
+
+public class Defect {
+	
+	public Long id;
+	
+	public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
